8f41d3100d743f24df1ab6314b1cf9943428999d
Victor Cornillere <covictor@inf.ethz.ch>
2020-08-04 12:56:19 +0200

    Add Readme

---
+++ README.md
@@ -0,0 +1,9 @@
+## Simple Rust Ray-tracer
+
+This repository hosts a simple ray-tracing framework written from scratch using Monte-Carlo integration.
+Heavily inspired by [PBRT](https://www.pbrt.org/) and [Nori](https://wjakob.github.io/nori/).
+
+![Deer rendered using Path Tracing](/results/deer.png "Rendered deer mesh using Path Tracing")
+
+
+
--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ use nalgebra::{Point3, Vector3};
 use ncollide3d::{
     math::Isometry,
     shape::{Cuboid, TriMesh},
+    transformation::ToTriMesh,
 };
 use obj::{Obj, SimplePolygon};
 use std::path::Path;
@@ -50,28 +51,42 @@ fn add_mesh_to_scene(scene: &mut Scene, obj_path: &Path) {
 }
 
 fn add_lights(scene: &mut Scene) {
-    let top_light = Cuboid::new(Vector3::new(3.0, 3.0, 0.1));
-    let top_transform = Isometry::translation(0.0, 0.0, 6.0);
-    let top_data = ObjectData {
-        emission: Some((2.0f32, Point3::new(1.0, 1.0, 1.0))),
-        ..Default::default()
-    };
-    scene
-        .add_shape(top_light)
-        .with_transform(top_transform)
-        .with_data(top_data)
-        .build();
+    //let mut top_light = Cuboid::new(Vector3::new(20.0f32, 20.0f32, 20.0f32)).to_trimesh(());
+    //top_light.flip_normals();
+    //let top_light_mesh = TriMesh::<f32>::from(top_light);
+    //let top_transform = Isometry::translation(0.0, 0.0, 0.0);
+    //let top_data = ObjectData {
+    //emission: Some((0.1f32, Point3::new(1.0, 1.0, 1.0))),
+    //..Default::default()
+    //};
+    //scene
+    //.add_shape(top_light_mesh)
+    //.with_transform(top_transform)
+    //.with_data(top_data)
+    //.build();
 
-    let left_light = Cuboid::new(Vector3::new(0.1, 3.0, 3.0));
-    let left_transform = Isometry::translation(6.0, 0.0, 0.0);
+    let left_light = Cuboid::new(Vector3::new(100.0, 0.1, 100.0));
+    let left_transform = Isometry::translation(0.0, 20.0, 0.0);
     let left_data = ObjectData {
-        emission: Some((2.0f32, Point3::new(0.4, 0.2, 0.6))),
+        emission: Some((1.0f32, Point3::new(0.4, 0.4, 0.8))),
         ..Default::default()
     };
     scene
         .add_shape(left_light)
         .with_transform(left_transform)
         .with_data(left_data)
+        .build();
+
+    let sun_light = Cuboid::new(Vector3::new(1.0, 1.0, 1.0));
+    let sun_transform = Isometry::translation(4.0, 8.0, 0.0);
+    let sun_data = ObjectData {
+        emission: Some((50.0f32, Point3::new(0.9, 0.8, 0.6))),
+        ..Default::default()
+    };
+    scene
+        .add_shape(sun_light)
+        .with_transform(sun_transform)
+        .with_data(sun_data)
         .build();
 }
 
@@ -82,7 +97,7 @@ fn main() {
     add_lights(&mut scene);
 
     scene.perform_collision_phase();
-    let samples = scene.capture(100u32);
+    let samples = scene.capture(2000u32);
 
     let mut image = RgbImage::new(samples.len() as u32, samples[0].len() as u32);
     let clamp = |x: f32| 1.0f32.min(0.0f32.max(x));
@@ -97,5 +112,5 @@ fn main() {
             ];
         }
     }
-    image.save("./output_cosine.png").unwrap();
+    image.save("./results/output_cosine.png").unwrap();
 }
